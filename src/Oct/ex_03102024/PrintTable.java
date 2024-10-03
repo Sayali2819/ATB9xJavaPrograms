85e79851dfda4bf037c79616fa2d7720092ec3a1
Sayali2819 <108470808+Sayali2819@users.noreply.github.com>
2024-10-03 12:17:15 +0530

    Update PrintTable.java

--- src/Oct/ex_03102024/PrintTable.java
+++ src/Oct/ex_03102024/PrintTable.java
@@ -1,4 +1,13 @@
 package Oct.ex_03102024;
 
-public class PrintTable {
+public class PrintTable 
+{
+   public static void main(String[] args) 
+   {
+        int num = 10;
+        for(int i=1; i<=10; i++)
+        {
+            System.out.println(num + "x" + i + "=" + (10*i));
+        }
+    }
 }
