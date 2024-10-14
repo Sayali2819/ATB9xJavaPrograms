70a38d01f5819ed0060ba01cbb7e047b84936e8f
Sayali Salvi <sayali.s@dibc.in>
2024-10-14 11:47:32 +0530

    Lab051

---
+++ src/Oct/ex_09102024/TableUsingPrintf.java
@@ -0,0 +1,16 @@
+package Oct.ex_09102024;
+
+public class TableUsingPrintf {
+    public static void main(String[] args) {
+        int num = 9;
+
+        for(int i=1; i<=10; i++)
+        {
+            System.out.printf("%d x %d = %d%n", num, i, num*i);
+        }
+    }
+}
+
+
+
+
---
+++ src/Oct/ex_14102024/Lab051.java
@@ -0,0 +1,13 @@
+package Oct.ex_14102024;
+
+public class Lab051 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b  = 12;
+        System.out.println(a%b);
+        System.out.println(10%12);
+
+        //10 < 12
+        //Division is not complete hence it will give default value
+    }
+}
